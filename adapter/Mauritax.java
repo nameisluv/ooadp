229135f65154b3b0d688dec4c265f9d8779dbfb5
Lava Kumar Gowda M A <91609093+nameisluv@users.noreply.github.com>
2023-05-31 00:15:16 +0530

    hehe

--- adapter/Mauritax.java
+++ adapter/Mauritax.java
@@ -1,7 +1,7 @@
 package ooadp.adapter;
 
 public class Mauritax {
-    public static double getVat(double amount)
+    public double getVat(double amount)
         {
             return amount* 0.15;
         }
